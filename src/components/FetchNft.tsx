602b2b041dd2f980c55c4e4e2c46d5acfdbf2894
Dinesh Atchuthan <dinesh.atchuthan@easymile.com>
2022-11-27 17:22:15 +0100

    added metaplex setup

--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -4,6 +4,12 @@ import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchNft: FC = () => {
+  const [nftData, setNftData] = useState(null)
+
+  const { connection } = useConnection()
+  const wallet = useWallet()
+  const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(wallet))
+
   const fetchNfts = async () => {}
 
   return <div></div>
